759bf39d9766e667fe887bb60d1ae9e859f8f45b
Rose <kennycastro007@gmail.com>
2023-04-21 21:31:24 -0400

    redesign log output

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,4 +1,9 @@
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
+
 exports.handler = async function () {
+  console.log(`${chalk.dim.grey(DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS))} ${chalk.bold.grey('LOG')}: Request to function hello-world received`);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,22 @@
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
 // mod.cjs
 // eslint-disable-next-line no-shadow, import/no-extraneous-dependencies
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 exports.handler = async (event) => {
+  const datePrefix = chalk.dim.grey(DateTime.now()
+    .toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS));
+  const logPrefix = `${datePrefix + chalk.bold.grey(' (LOG)')}: `;
   const eventBody = JSON.parse(event.body);
   const POKE_API = `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`;
 
+  console.log(logPrefix + chalk.white('Pokedex function called - ') + chalk.green(`Region requested: ${JSON.stringify(eventBody)}`));
+
   const response = await fetch(POKE_API);
   const data = await response.json();
+
+
 
   return {
     statusCode: 200,
